ba4cddf76bf639c7b1a3b62ca0a748484debb57e
Shahroon Lodhi <251706895@formanite.fccollege.edu.pk>
2025-05-31 14:40:28 +0500

    Set up Cloudinary upload provider and fixed TS import path

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,13 +3,13 @@ export default ({ env }) => ({
     store: env('SHOPIFY_STORE'),
     token: env('SHOPIFY_TOKEN'),
   },
-upload: {
+  upload: {
     config: {
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env('dnvi0oure'),
-        api_key: env('482846687733961'),
-        api_secret: env('UudK8D4GwQUwV-3Hra60AkBToy8'),
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
       },
       actionOptions: {
         upload: {},
